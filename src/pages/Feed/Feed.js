30bb0e6e82d20e7310aba210cc3843d98fb51f1b
Sarunas <sarunas.lekstutis@gmail.com>
2021-09-07 08:57:39 +0300

    pagination

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -69,7 +69,7 @@ class Feed extends Component {
     const graphqlQuery = {
       query: `
         {
-            posts {
+            posts(page: ${page}) {
               posts {
                 _id
               title
@@ -233,6 +233,7 @@ class Feed extends Component {
             );
             updatedPosts[postIndex] = post;
           } else {
+            updatedPosts.pop();
             updatedPosts.unshift(post);
           }
           return {
